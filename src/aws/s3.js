5c234e51887207f3e7662a84a5da389ab29d1e5a
ajithhegde2 <ajith.hegde2@gmail.com>
2024-01-10 22:45:16 +0530

    final

--- src/aws/s3.js
+++ src/aws/s3.js
@@ -5,17 +5,17 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3")
 
  const sendToS3 = async (file) => {
 const client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.MY_AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
   },
 })
 
   
   
   const command = new PutObjectCommand({
-    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Bucket: process.env.MY_AWS_S3_BUCKET_NAME,
     Key: Date.now().toString(),
     Body: file.buffer, // Assuming file.buffer contains the file data
     ContentType: file.mimetype,
