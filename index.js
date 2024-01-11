fcf1721a586c2b01a222c1d11ebf91a13a6597dc
ajithhegde2 <ajith.hegde2@gmail.com>
2024-01-11 12:01:04 +0530

    final

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ require('dotenv').config()
 const multer = require('multer')
 const storage = multer.memoryStorage() // Use in-memory storage for handling files as buffers
 const upload = multer({ storage })
+require('./src/db/db.connect')
+
+const Image= require('./src/model/image')
+
+Image.sync({ alter: true })
+
+
+
 
 const app = express()
 app.use(cors())
@@ -12,9 +20,29 @@ app.use(express.json({ limit: '5mb' }));
 
 app.post('/api',upload.any(), async(req,res,next)=>{
 
-   const response = await sendToS3(req.files[0])
-   console.log(response);
-  res.json(response)
+  try {
+     const response = await sendToS3(req.files[0])
+
+     const image = await Image.create({ key: response })
+
+     res.json(image)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+app.get('/api', async (req, res, next) => {
+  try {
+   
+
+    const image = await Image.findAll()
+
+    
+
+    res.json(image)
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
 
---
+++ src/model/image.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize')
+const { sequelize } = require('../db/db.connect')
+
+
+const Image = sequelize.define(
+  'Image',
+  {
+    // Model attributes are defined here
+    key: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+   
+  },
+  {
+    // Other model options go here
+    tableName:'Images'
+  }
+)
+
+// `sequelize.define` also returns the model
+console.log(Image === sequelize.models.Image) // true
+
+module.exports = Image
